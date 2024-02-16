155f5c9a3c7fa9e6c0c81fc49009366faaf4e61a
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-02-16 08:06:23 +0800

    Add files via upload

--- public/auth.js
+++ public/auth.js
@@ -1,18 +1,33 @@
 import { useEffect, useState } from 'react'
 import { Authentication } from './firebaseConfig'
 import { InitialUserState, useUser } from './user'
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { db } from './firebaseConfig';
+
+
+async function fetchData_ModelUser() {
+    const querySnapshot = await getDocs(collection(db, "model_user"));
+    const data = [];
+    querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+    });
+    return data;
+}
 
 const AuthStateChangeProvider = ({ children }) => {
     const user = useUser()
     const { SetUser } = user
     const InitiateAuthStateChange = () => {
-        Authentication().onAuthStateChanged((user) => {
+        Authentication().onAuthStateChanged(async (user) => {
             if (user) {
-                console.log('Anda Sudah Login')
-                // console.log(user)
-                SetUser({ email: user.email, uid: user.uid })
+                console.log('ini Anda Sudah Login')
+
+                const data = await fetchData_ModelUser();
+                const userData = data.find(DbUser => DbUser.email === user.email);
+
+                SetUser({ email: user.email, uid: user.uid, role: userData.role })
             } else {
-                console.log('Anda Belum Login')
+                console.log('ini Anda Belum Login')
                 SetUser(InitialUserState)
             }
         })
