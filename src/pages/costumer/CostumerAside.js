7d419b96a2925cbdcd13c92acfc5c862dfefd15b
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-02-19 01:10:42 +0800

    Add files via upload

--- src/pages/costumer/CostumerAside.js
+++ src/pages/costumer/CostumerAside.js
@@ -31,6 +31,12 @@ export default function CashierAside({ isTransaksiActive, isProdukActive, handle
         }
     };
 
+    const handleLogout = async () => {
+        await SignOut();
+        // Reload halaman setelah logout
+        window.location.reload();
+    };
+
     console.log("ini profile", profile)
     return (
         <>
@@ -60,7 +66,7 @@ export default function CashierAside({ isTransaksiActive, isProdukActive, handle
                         </Link>
                     </span>
                     <span style={{ width: '80%' }}>
-                        <button className='logout' onClick={SignOut}>Logout</button>
+                        <button className='logout' onClick={handleLogout}>Logout</button>
                     </span>
                 </section>
             </aside>
