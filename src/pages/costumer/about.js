4ea14c164dc06a2e0827184dfc1d4a32a3d2f734
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-03-18 22:19:37 +0800

    Add files via upload

--- src/pages/costumer/about.js
+++ src/pages/costumer/about.js
@@ -22,9 +22,14 @@ export default function About() {
                         <h1>Nur Afia Store</h1>
                     </span>
                     <span>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-                            electronic typesetting, remaining essentially unchanged</p>
+                        <p style={{ textAlign: 'justify' }}>Minimarket Nur Afiah yang berlokasi di RS. Wahidin Sudirohusodo adalah sebuah usaha menengah
+                            yang menyediakan berbagai kebutuhan sehari-hari bagi para pengunjung rumah sakit dan masyarakat sekitar.
+                            Dengan suasana yang ramah dan nyaman, minimarket ini menjadi tempat yang praktis bagi para pasien,
+                            pihak rumah sakit, serta para konsumen lainnya untuk memenuhi kebutuhan mereka tanpa harus meninggalkan
+                            area rumah sakit. Di dalam minimarket ini, Anda akan menemukan beragam produk mulai dari makanan ringan,
+                            minuman, bahan makanan pokok, kebutuhan keseharian, buah buahan, hingga perlengkapan kebersihan. pilihannya
+                            cukup lengkap untuk memenuhi kebutuhan sehari-hari. Dengan menyediakan produk-produk berkualitas dan harga yang
+                            bersaing, Nur Afiah berusaha memberikan layanan terbaik kepada para Konsumen.</p>
                     </span>
                     <span style={{ marginTop: '30px' }}>
                         <button onClick={handleGoBack}>Lanjutan</button>
